8855bb716258956a9acb11a215bb625fc9758f67
gershon.araque <geraquem@gmail.com>
2022-09-21 09:20:58 +0200

    wip

--- app/src/main/java/com/mmfsin/flashjuice/MainActivity.kt
+++ app/src/main/java/com/mmfsin/flashjuice/MainActivity.kt
@@ -16,6 +16,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity(), IListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(1500)
+        setTheme(R.style.Theme_FlashJuice)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
